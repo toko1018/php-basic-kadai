959d472d311ab26ae60668c2b8e430c4e3ce3a77
toko1018 <toko.sa10mi@gmail.com>
2025-01-31 22:26:06 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -14,28 +14,26 @@
         {
             if ($order == "asc") {
                 sort($array);
-            } else if ($order == "desc") {
+                echo "昇順にソートします。<br>";
+            } 
+            else if ($order == "desc") {
                 rsort($array);
+                echo "降順にソートします。<br>";
             }
-            return $array;
+            
+            foreach ($array as $num) {
+                echo $num . "<br>";
+            }
         }
         
         // ソートする配列を宣言
         $nums = [15, 4, 18, 23, 10];
         
-        // 昇順にソート
-        echo "昇順にソートします。<br>";
-        $sorted_nums_asc = sort_2way($nums, "asc");
-        foreach ($sorted_nums_asc as $num) {
-            echo $num . "<br>";
-        }
+        // 昇順にソートして出力
+        sort_2way($nums, "asc");
         
-        // 降順にソート
-        echo "降順にソートします。<br>";
-        $sorted_nums_desc = sort_2way($nums, "desc");
-        foreach ($sorted_nums_desc as $num) {
-            echo $num . "<br>";
-        }
+        // 降順にソートして出力
+        sort_2way($nums, "desc");
         
         ?>
     </p>
